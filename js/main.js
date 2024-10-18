2257b073a9db417a65f5ec7af42d5125f4ee6b8e
emazupp <zuppardo.emanuele2001@gmail.com>
2024-10-18 14:53:07 +0200

    test

--- js/main.js
+++ js/main.js
@@ -6,10 +6,23 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 // ! SVOLGIMENTO
 
-function isPalindrome(word) {}
+function convertToArray(word) {
+  const arrayWord = [...word];
+  return arrayWord;
+}
+
+function isPalindrome(word) {
+  let palindrome = true;
+  for (let i = 0; i < word.length && palindrome; i++) {
+    word[i] == word[word.length - 1 - i];
+    console.log("word[i]: " + word[i]);
+    console.log("word[word.length - 1 - i]: " + word[word.length - 1 - i]);
+  }
+}
 
 const word = "anna";
-console.log(word[0]);
+let wordConverted = convertToArray(word);
+isPalindrome(wordConverted);
 
 // ! TRACCIA
 
